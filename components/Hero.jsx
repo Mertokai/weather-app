65a5184108f69fcd7e5c5cd6b5e6e8a0dd90b249
Mertokai <merteyuboglu.16@hotmail.com>
2023-04-11 09:39:14 +0300

    mobile appropriate

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -26,18 +26,20 @@ function Hero() {
 
   return (
     <div className="bg-slate-50 h-screen space-y-5">
-      <div className="text-center py-10 my-auto ">
-        <form className="space-x-5 " onSubmit={handleSubmit}>
-          <label className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md" htmlFor="searchTerm">
-            Şehir ismi:
-          </label>
-          <input
-            className="hadow appearance-none border border-blue-500 rounded  py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            id="searchTerm"
-            value={searchTerm}
-            onChange={(event) => setSearchTerm(event.target.value)}
-          />
+      <div className="text-center py-10 my-auto items-center">
+        <form className="space-x-5 flex justify-center px-3 " onSubmit={handleSubmit}>
+          <div className="flex space-x-5 items-center">
+            <label className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md  " htmlFor="searchTerm">
+              Şehir ismi:
+            </label>
+            <input
+              className="hadow appearance-none border border-blue-500 rounded  py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              id="searchTerm"
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+          </div>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" type="submit">
             Button
           </button>
